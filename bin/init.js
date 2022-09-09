99141505e6bac0abdac6634bf6a84ffade49786f
jiaao.Mario <jiaao.mario@gmail.com>
2022-09-10 02:24:17 +0800

    feat: 📠 add prettier and lintstaged rc

--- bin/init.js
+++ bin/init.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
-const fs = require("fs");
 const path = require("path");
 const husky = require("husky");
+const semver = require("semver");
+const whichPMRuns = require("which-pm-runs");
 const childProcess = require("child_process");
+const pkg = require("../package.json");
 
 const { execSync } = childProcess;
 const commandCallPath = process.env.INIT_CWD;
 const huskyRootPath = path.join(commandCallPath, ".husky");
-const preCommitPath = path.join(huskyRootPath, ".pre-commit");
-const commitMsgPath = path.join(huskyRootPath, ".commit-msg");
+const preCommitPath = path.join(huskyRootPath, "pre-commit");
+const commitMsgPath = path.join(huskyRootPath, "commit-msg");
 try {
   execSync("git rev-parse --git-dir >/dev/null 2>&1");
   husky.install();
@@ -17,4 +19,36 @@ try {
 } catch (error) {
   console.log("🆘 oops~ it seems that your project didn't initialization git.");
   console.log("suggest: you can execute git init first");
+}
+
+const pmRunner = whichPMRuns();
+const pm = pmRunner.name;
+const expectMinNodeVersion = semver.minVersion(pkg.engines.node);
+try {
+  function _checkPMVersion() {
+    return semver.lt(process.version, expectMinNodeVersion);
+  }
+
+  if (_checkPMVersion()) {
+    throw Error("nodeVersion");
+  }
+  console.log(pm);
+  switch (pm) {
+    case "npm":
+      execSync("npm install lint-staged prettier --save-dev");
+      break;
+    case "yarn":
+    case "pnpm":
+      execSync(`${pm} add lint-staged prettier --save-dev`);
+      break;
+  }
+} catch (error) {
+  if (
+    new Error(error).message &&
+    new Error(error).message === "Error: nodeVersion"
+  ) {
+    console.log(
+      `🆘 oops~ your current node version: ${process.version} is lower than you expect min node version: ${expectMinNodeVersion}`
+    );
+  }
 }
---
+++ prettier.js
@@ -0,0 +1,5 @@
+/*
+ * @Author: mario.ma
+ * @Date: 2022-09-10 02:12:48
+ */
+module.exports = require("./lib").prettierrc;
